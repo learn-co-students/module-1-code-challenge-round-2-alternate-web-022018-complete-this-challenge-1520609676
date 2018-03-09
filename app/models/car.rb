4e95691e952bc00f2745213102d8a7cdb71aa7f7
github username <github email address>
2018-03-09 11:49:31 -0500

    york

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,13 @@
 class Car
-  
+  attr_accessor  :passenger_count, :moviescreen
+  @@all = []
+
   def initialize(passenger_count)
     @passenger_count = passenger_count
+    @@all << self
+  end
+
+  def self.all
+    @@all
   end
 end
--- app/models/movie_screen.rb
+++ app/models/movie_screen.rb
@@ -1,8 +1,41 @@
 class MovieScreen
+  attr_accessor :movie, :capacity
+  @@all = []
 
   def initialize(movie, capacity)
     @movie = movie
     @capacity = capacity
+
+    @@all << self
+  end
+
+  def self.all_screens
+    @@all
+  end
+
+  def cars
+    Car.all.select {|c| c.moviescreen == self}
+  end
+
+  def at_capacity
+    cars.count == self.capacity
+  end
+
+  def add_car
+    if at_capacity == true
+      "Movie is sold out"
+    else
+      ncar = Car.new(4)
+      ncar.moviescreen = self
+      "Enjoy!"
+    end
+  end
+
+  def how_many_viewers
+    viewers_array = cars.map {|c| c.passenger_count}
+    sum = 0
+    viewers_array.each {|i| sum += i }
+    sum
   end
 
 end
